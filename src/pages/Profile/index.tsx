1e91f6c0f9874d9fe95fd9dd8a9142c861933255
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-02-18 19:37:41 -0300

    create Profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,48 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import { Container, Main, LeftSide, RightSide, Repos } from './styles';
 
-// import { Container } from './styles';
+import ProfileData from '../../components/ProfileData';
+import RepoCard from '../../components/RepoCard';
 
-const Profile: React.FC = () => {
-  return <div />;
-};
+function Profile() {
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username="ssk4trio"
+            name="Keven de Sousa Silva"
+            avatarUrl="https://avatars.githubusercontent.com/u/48361908?v=4"
+            followers={213}
+            following={122}
+            company="Bagy"
+            location="Parnaiba, Piauí, Brasil "
+            email="keven.sousa@bagy.com.br"
+            blog="https://www.linkedin.com/in/keven-de-sousa-silva-00bb85201/"
+          />
+        </LeftSide>
+        <RightSide>
+          <Repos>
+            <h2>Random Repos</h2>
+            <div>
+              {[1, 2, 3, 4, 5, 6].map((n) => (
+                <RepoCard
+                  key={n}
+                  username="ssk4trio"
+                  reponame="github-clone"
+                  description="Clone do Github"
+                  language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
+                  stars={12}
+                  forks={12}
+                />
+              ))}
+            </div>
+          </Repos>
+        </RightSide>
+      </Main>
+    </Container>
+  );
+}
 
 export default Profile;
